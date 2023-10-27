7e66057851f5d32be1dcc167d476697e739d5923
morachake <jacobmoracha@gmail.com>
2023-10-28 01:43:51 +0300

    updates adding linters

--- src/constants/Index.js
+++ src/constants/Index.js
@@ -1,6 +1,10 @@
 export const COLORS = {
-    primary: '#388479',
-    lightPrimary: 'rgba(56, 132, 121, 0.3)',
-    grey: '#D9D9D9',
-    background: '#F5F5F5',
-  };
+  primary: "#388479",
+  lightPrimary: "rgba(56, 132, 121, 0.3)",
+  grey: "#D9D9D9",
+  background: "#F5F5F5",
+};
+
+export const IMAGES = {
+  logo: require("../assets/images/Safialogo.png"),
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,47 @@
-import React from 'react'
-import {  StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { COLORS, IMAGES } from "../constants/Index";
 export default function Login() {
   return (
-    <SafeAreaView  style={{flex:1}}>
-     <View>
-      <Text>Login to your account</Text>
-     </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ paddingTop: 10 }}>
+        <View style={styles.container}>
+          <Feather name="chevron-left" size={26} color="black" />
+          <Image source={IMAGES.logo} style={styles.image} />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              alignItems: "center",
+              color: COLORS.primary,
+              fontSize: 32,
+              fontFamily: "Medium",
+            }}
+          >
+            Login to your account
+          </Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <TextInput placeholder="Type :" />
+        </View>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  // container : {
-  //   flex:1,
-  //   paddingTop : insets.top
-  // }
-})
+  container: {
+    display: "flex",
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    width: 61,
+    height: 45,
+  },
+});
