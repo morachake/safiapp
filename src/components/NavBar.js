dfd1fa619fb23bc2e520e0b1e1f1df747aaf8a9f
morachake <jacobmoracha@gmail.com>
2023-11-02 01:07:09 +0300

    last update tonight

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View ,Text} from 'react-native';
+import { Entypo , Octicons} from '@expo/vector-icons';
 
+import { COLORS } from '../constants/Index';
 export default function NavBar() {
   return (
-    <View>
-        
+    <View style={{flexDirection:'row'}}>
+        <Entypo name="menu" size={24} color="black" />
+        <Text>Home</Text>
+        <Octicons name="filter" size={24} color="black" />
     </View>
   );
 }
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-   <View>
-    <Text>Home</Text>
-   </View>
-  )
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
 }
