e8b0848bf47247b8ae775462d7367e0e49c21188
morachake <jacobmoracha@gmail.com>
2023-11-02 00:42:09 +0300

    updating drawer menu content

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { EvilIcons } from '@expo/vector-icons';
-
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { EvilIcons, SimpleLineIcons } from '@expo/vector-icons';
+import COLORS from "../constants/Index"
 export default function CustomDrawer(props) {
     return (
         <View style={styles.container}>
@@ -14,15 +14,25 @@ export default function CustomDrawer(props) {
                     <EvilIcons name="user" size={84} color="#FFFF" />
                     <Text style={{ color: "#FFFF", fontFamily: "Medium", fontSize: 25 }}>Moracha</Text>
                 </View>
-                <View style={{ flex: 1, paddingTop: 10 }}>
+                <View style={{ flex: 1, paddingTop: 15 }}>
                     <DrawerItemList {...props} />
                 </View>
 
-                <View style={{paddingTop:20}}>
-                    <View style={{justifyContent:"center",alignItems:"center" ,borderBottomWidth:1}}>
-                         <Text style ={{padding:20}}>Tell Us What you think</Text> 
+                <View style={{ paddingTop: 25 }}>
+                    <TouchableOpacity style={{ justifyContent: "center", alignItems: "center", borderBottomWidth: 1 }}>
+                        <Text style={{ padding: 20, fontFamily: "Medium" }}>Tell Us What you think</Text>
+                    </TouchableOpacity>
+                    <View style={{ justifyContent: "center", alignItems: "center" }}>
+                        <TouchableOpacity style={{ color: "#FFFF"}}>
+                             <Text style={{ padding: 20, fontFamily: "Medium" }}>Help Us imporove</Text>
+                        </TouchableOpacity> 
                     </View>
-                   
+                </View>
+                <View style={styles.bottomcont}>
+                    <TouchableOpacity style={{flexDirection:"row"}}>
+                        <SimpleLineIcons name="logout" size={24} color="black" />
+                        <Text style={{fontSize:14, fontFamily: "Bold",marginLeft:8}}>Log Out</Text>
+                    </TouchableOpacity>
                 </View>
             </DrawerContentScrollView>
         </View>
@@ -33,12 +43,22 @@ export default function CustomDrawer(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#D9D9D9"
+        backgroundColor: "#D9D9D9",
     },
-    topcontainer :{ 
+    topcontainer: {
         backgroundColor: "#388479",
-         height: 140,
-          alignItems: "center", 
-          justifyContent: "space-around"
+        height: 140,
+        alignItems: "center",
+        justifyContent: "space-around"
+    },
+    bottomcont: {
+        flexDirection: "row",
+        justifyContent: "flex",
+        alignItems: "center",
+        marginVertical:2,
+        borderTopWidth: 4,
+        borderBottomWidth: 4,
+        padding: 20,
+        borderColor: "#388479",
     }
 })
